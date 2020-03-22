48e9d60266f10fcebc756bb21038e0b31a66b6e5
Magda Mejía <magda.mejia96@gmail.com>
2020-03-22 13:26:48 -0500

    Test fixed (VERDE)

--- portfolioTddProject/portfolio/tests.py
+++ portfolioTddProject/portfolio/tests.py
@@ -3,10 +3,10 @@ from django.test import TestCase, Client
 
 
 # Create your tests here.
-class GalleryTestCase(TestCase):
+class PortfolioTestCase(TestCase):
 
     def test_list_portfolios_status(self):
-        url = '/portfolio'
+        url = '/portfolio/'
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, 200)
 
--- portfolioTddProject/portfolio/views.py
+++ portfolioTddProject/portfolio/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
-from .models import Image
+from .models import Portfolio
 import json
 
+
 # Create your views here.
+@csrf_exempt
+def index(request):
+    portfolio_list = Portfolio.objects.all()
+    return HttpResponse(serializers.serialize("json", portfolio_list))
