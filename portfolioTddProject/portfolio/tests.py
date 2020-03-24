f9d8e4a9e0593f50772ee9160207fd3cc6ed8ae0
Magda Mejía <magda.mejia96@gmail.com>
2020-03-24 14:35:02 -0500

    remove __pycache__

--- portfolioTddProject/portfolio/tests.py
+++ portfolioTddProject/portfolio/tests.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
 from django.test import TestCase, Client
-from .models import Image,Portfolio,UserP
+from .models import Image, Portfolio, UserP
 import json
+
 
 # Create your tests here.
 class PortfolioTestCase(TestCase):
@@ -12,11 +13,12 @@ class PortfolioTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_list_portfolios_count(self):
-        user_model = User.objects.create_user(username='test', password='kd8wke-DE34', first_name='test', last_name='test', email='test@test.com')
-        user_p = UserP.objects.create(user=user_model,photo='', professional_profile= 'profProf1')
+        user_model = User.objects.create_user(username='test', password='kd8wke-DE34', first_name='test',
+                                              last_name='test', email='test@test.com')
+        user_p = UserP.objects.create(user=user_model, photo='', professional_profile='profProf1')
         image = Image.objects.create(name='nuevo', url='No', description='testImage', type='private')
         Portfolio.objects.create(name='nuevo', description='testImage', image=image, user=user_p)
-        Portfolio.objects.create(name='nuevo2', description='testImage', image=image,  user=user_p)
+        Portfolio.objects.create(name='nuevo2', description='testImage', image=image, user=user_p)
         Portfolio.objects.create(name='nuevo3', description='testImage', image=image, user=user_p)
 
         url = '/portfolio/'
@@ -24,3 +26,11 @@ class PortfolioTestCase(TestCase):
         current_data = json.loads(response.content)
         print(current_data)
         self.assertEqual(len(current_data), 3)
+
+    def test_add_user(self):
+        response = self.client.post('/portfolio/addUserP/', json.dumps(
+            {"username": "magda26", "first_name": "Magda", "last_name": "Mejía", "password": "AnyPas#5",
+             "email": "test@test.com",
+             "photo": '', "professional_profile": 'profProf1'}), content_type='application/json')
+        current_data = json.loads(response.content)
+        self.assertEqual(current_data[0]['fields']['username'], 'magda26')
